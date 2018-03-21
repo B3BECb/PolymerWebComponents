ac40ad3d01b9f70bd5dc21f55d71970918ee61b1
danith <danith@mallenom.ru>
2018-03-21 17:50:30 +0300

    reformating dots menu

--- PolymerDotsMenu/PolymerDotsMenu.js
+++ PolymerDotsMenu/PolymerDotsMenu.js
@@ -23,9 +23,9 @@ class DotsMenu
 	static get properties()
 	{
 		return {
-			settings : Boolean,
-			disabled : Boolean,
-			items : {
+			settings: Boolean,
+			disabled: Boolean,
+			items:    {
 				value()
 				{
 					return [];
@@ -37,14 +37,9 @@ class DotsMenu
 	_fireCallback(args)
 	{
 		if(args.currentTarget.callback)
+		{
 			args.currentTarget.callback();
-	}
-
-	_OnCustomIconLoaded()
-	{
-		//FIXME: переписать на промисы
-		let iconContainer = Polymer.dom(this.root).querySelector('#customIcon' + (this.items.length - 1));
-		iconContainer.appendChild(iconContainer.icon);
+		}
 	}
 
 	AddItem(value, callback = null, type = null)
@@ -56,25 +51,66 @@ class DotsMenu
 			text = value[0];
 			icon = value[1];
 		}
-		this.items.push(
+		/*this.items.push(
 			{
-				value: text,
+				value:    text,
 				callback: callback,
 				type:
-					{
-						remove : type == "remove" ? true : false,
-						edit : type == "edit" ? true : false,
-						switchView : type == "switchView" ? true : false,
-						custom : type == "custom" ? true : false,
-					},
-				icon: icon,
-			}
+						  {
+							  remove:     type == "remove" ? true : false,
+							  edit:       type == "edit" ? true : false,
+							  switchView: type == "switchView" ? true : false,
+							  custom:     type == "custom" ? true : false,
+						  },
+				icon:     icon,
+			},
 		);
 
 		let list = this.items;
 		this.items = [];
-		this.items = list;
+		this.items = list;*/
 
-		//FIXME: Должен возвращать добавленный элемент. Переписать на промисы!
+		let getInsertedItem = async() =>
+		{
+			await
+				new Promise((resolve, reject) =>
+					{
+						var waitEvent = (args) =>
+						{
+							this.$.repeater.removeEventListener('dom-change', waitEvent);
+							resolve();
+						};
+
+						this.$.repeater.addEventListener('dom-change', waitEvent);
+
+						this.push('items', {
+							value:    text,
+							callback: callback,
+							type:
+									  {
+										  remove:     type == "remove" ? true : false,
+										  edit:       type == "edit" ? true : false,
+										  switchView: type == "switchView" ? true : false,
+										  custom:     type == "custom" ? true : false,
+									  },
+							icon:     icon,
+						});
+					},
+				);
+
+			return Polymer.dom(this.root).querySelector('#customIcon' + (this.items.length - 1));
+		};
+
+		let aa = Promise.all([getInsertedItem()]);
+
+		return aa.then(
+			(item) =>
+			{
+				item.appendChild(item.icon);
+
+				return Polymer.dom(this.root).querySelector('#item' + (this.items.length - 1))
+			}
+		);
+
 	}
 }
